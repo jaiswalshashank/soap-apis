9c538aa1599f04c3a2bfe9dfcafb2f5246771ac4
shashankjai2012@gmail.com <shashankjai2012@gmail.com>
2021-07-07 21:58:00 +0530

    git ignore

--- src/main/java/com/shasha/soapapis/config/ApplicationConfig.java
+++ src/main/java/com/shasha/soapapis/config/ApplicationConfig.java
@@ -1,91 +1,63 @@
 /*
 package com.shasha.soapapis.config;
 
-import javax.xml.ws.Endpoint;
-
-import com.shasha.soapapis.dataacess.webservicesserver.NumberConversionSoapType;
-import org.apache.cxf.Bus;
-import org.apache.cxf.bus.spring.SpringBus;
-import org.apache.cxf.ext.logging.LoggingFeature;
-import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.transport.servlet.CXFServlet;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.server.EndpointInterceptor;
-import org.springframework.ws.soap.security.wss4j2.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
+import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
 
 import java.util.Collections;
 import java.util.List;
 
-
-@Configuration
 @EnableWs
+@Configuration
 public class ApplicationConfig extends WsConfigurerAdapter {
 
-	@Override
-	public void addInterceptors(List<EndpointInterceptor> interceptors){
-		interceptors.add(securityInterceptor());
-	}
+    @Bean
+    public ServletRegistrationBean cfxServlet() {
+        return new ServletRegistrationBean<>(new CXFServlet(), "/soap-api/*");
+    }
 
-	@Bean
-	public XwsSecurityInterceptor securityInterceptor() {
-		XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
-		securityInterceptor.setCallbackHandler(callbackHandler());
-		securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
-		return securityInterceptor;
-	}
+    @Bean(name = "ticketagent")
+    public Wsdl11Definition defaultWsdl11Definition() {
+        SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
+        wsdl11Definition.setWsdl(new ClassPathResource("/wsdl/sample.wsdl"));
 
-	@Bean
-	public SimplePasswordValidationCallbackHandler callbackHandler() {
-		SimplePasswordValidationCallbackHandler callbackHandler = new SimplePasswordValidationCallbackHandler();
-		callbackHandler.setUsersMap(Collections.singletonMap("admin", "pwd123"));
-		return callbackHandler;
-	}
+        return wsdl11Definition;
+    }
 
-	@Bean
-	public ServletRegistrationBean<CXFServlet> dispatcherServlet() {
-		return new ServletRegistrationBean<>(new CXFServlet(), "/soap-api/*");
-	}
-	
-	@Bean
-	@Primary
-	public DispatcherServletPath dispatcherServletPathProvider() {
-	    return () -> "";
-	}
 
-	@Bean(name=Bus.DEFAULT_BUS_ID)
-	public SpringBus springBus(LoggingFeature loggingFeature) {
-		
-		SpringBus cxfBus = new  SpringBus();
-		cxfBus.getFeatures().add(loggingFeature);
-		
-		return cxfBus;
-	}
+    @Bean
+    public XwsSecurityInterceptor securityInterceptor() {
+        XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+        securityInterceptor.setCallbackHandler(callbackHandler());
+        securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
+        return securityInterceptor;
+    }
 
-	@Bean
-	public LoggingFeature loggingFeature() {
-		
-		LoggingFeature loggingFeature = new LoggingFeature();
-		loggingFeature.setPrettyLogging(true);
-		
-		return loggingFeature;
-	}
+    @Bean
+    SimplePasswordValidationCallbackHandler callbackHandler() {
+        SimplePasswordValidationCallbackHandler callbackHandler = new SimplePasswordValidationCallbackHandler();
+        // TODO @rap: Use real username and passwords
+        callbackHandler.setUsersMap(Collections.singletonMap("user", "password"));
+        return callbackHandler;
+    }
 
-	@Bean
-	public Endpoint endpoint(Bus bus, NumberConversionSoapType numberConversionSoapType) {
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
+    }
+}
 
-		EndpointImpl endpoint = new EndpointImpl(bus, numberConversionSoapType);
-		endpoint.publish("/service/accounts");
-		
-		return endpoint;
-	}
 
-}*/
+*/
--- src/test/java/com/shasha/soapapis/SoapApisApplicationTests.java
+++ src/test/java/com/shasha/soapapis/SoapApisApplicationTests.java
@@ -1,3 +1,4 @@
+/*
 package com.shasha.soapapis;
 
 import org.junit.jupiter.api.Test;
@@ -11,3 +12,4 @@ class SoapApisApplicationTests {
 	}
 
 }
+*/
